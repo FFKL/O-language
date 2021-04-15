4be42cd0240e5a8bf58a9bea9258110b0e9beec6
dmitrii <ffklffkl@gmail.com>
2021-04-16 00:42:36 +0300

    add names table

---
+++ OTable.pas
@@ -0,0 +1,103 @@
+
+Unit OTable;
+
+Interface
+
+Uses OScan;
+
+Type 
+  tCat = (catConst, catVar, catType, catStProc, catModule, catGuard);
+  tType = (typNone, typeInt, typBool);
+  tObj = ^tObjRec;
+  tObjRec = Record
+    Name: tName;
+    Cat: tCat;
+    Typ: tType;
+    Val: integer;
+    Prev: tObj;
+  End;
+
+Procedure InitNameTable;
+Procedure Enter(N: tName; C: tCat; T: tType; V: integer);
+Procedure NewName(Name: tName; Cat: tCat; Var Obj: tObj);
+Procedure Find(Name: tName; Var Obj: tObj);
+Procedure OpenScope;
+Procedure CloseScope;
+
+Implementation
+
+Var 
+  Top: tObj;
+  Bottom: tObj;
+
+Procedure InitNameTable;
+Begin
+  Top := Nil;
+End;
+
+Procedure Enter(N: tName; C: tCat; T: tType; V:integer);
+
+Var 
+  P: tObj;
+Begin
+  New(P);
+  P^.Name := N;
+  P^.Cat := C;
+  P^.Typ := T;
+  P^.Val := V;
+  P^.Prev := Top;
+  Top := P;
+End;
+
+Procedure OpenScope;
+Begin
+  Enter('', catGuard, typNone, 0);
+  If Top^.Prev = Nil Then
+    Bottom := Top;
+End;
+
+Procedure CloseScope;
+
+Var 
+  P: tObj;
+Begin
+  While Top^.Cat <> catGuard Do
+    Begin
+      P := Top;
+      Top := Top^.Prev;
+      Dispose(P);
+    End;
+  P := Top;
+  Top := Top^.Prev;
+  Dispose(P);
+End;
+
+Procedure NewName(Name: tName; Cat: tCat; Var Obj: tObj);
+Begin
+  Obj := Top;
+  While (Obj^.Cat <> catGuard) And (Obj^.Name <> Name) Do
+    Obj := Obj^.Prev;
+  If Obj^.Cat = catGuard Then
+    Begin
+      New(Obj);
+      Obj^.Name := Name;
+      Obj^.Cat := Cat;
+      Obj^.Val := 0;
+      Obj^.Prev := Top;
+      Top := Obj;
+    End
+  Else
+    Error('Name already defined');
+End;
+
+Procedure Find(Name: tName; Var Obj: tObj);
+Begin
+  Bottom^.Name := Name;
+  Obj := Top;
+  While Obj^.Name <> Name Do
+    Obj := Obj^.Prev;
+  If Obj = Bottom Then
+    Error('Undeclared name');
+End;
+
+End.
